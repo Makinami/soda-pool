b13f08571d937b0775751d9e3f206a968cdda27d
Wiktoria Klonowska <wiktoria@makinami.im>
2025-03-06 23:17:41 +0900

    Add internal server error chance to example server

--- example-server/src/main.rs
+++ example-server/src/main.rs
@@ -9,7 +9,7 @@ use example_protobuf::{
     echo_server::{Echo, EchoServer},
     health_server::{Health, HealthServer},
 };
-use rand::random_range;
+use rand::{random_range, Rng};
 use tokio::time::sleep;
 
 const PORT: u16 = 50001;
@@ -17,13 +17,14 @@ const PORT: u16 = 50001;
 #[tokio::main]
 async fn main() {
     let mut tasks = HashMap::new();
+    let mut reliability = 0.9;
 
     macro_rules! start_server {
         ($address:expr) => {
             let (tx, rx) = tokio::sync::oneshot::channel::<()>();
             let server = tokio::spawn(async move {
                 tonic::transport::Server::builder()
-                    .add_service(HealthServer::new(HealthImpl { address: $address }))
+                    .add_service(HealthServer::new(HealthImpl { address: $address, reliability }))
                     .add_service(EchoServer::new(EchoImpl {}))
                     .serve_with_shutdown($address, async {
                         rx.await.ok();
@@ -94,14 +95,32 @@ async fn main() {
             }
             "help" => {
                 println!("Available commands:");
-                println!("  start v4   Start server on IPv4 localhost");
-                println!("  start v6   Start server on IPv6 localhost");
-                println!("  stop v4    Stop server on IPv4 localhost");
-                println!("  stop v6    Stop server on IPv6 localhost");
-                println!("  list       List running servers");
-                println!("  exit       Exit the program");
+                println!("  start v4               Start server on IPv4 localhost");
+                println!("  start v6               Start server on IPv6 localhost");
+                println!("  stop v4                Stop server on IPv4 localhost");
+                println!("  stop v6                Stop server on IPv6 localhost");
+                println!("  reliability <value>    Set server reliability (0.0-1.0)");
+                println!("  list                   List running servers");
+                println!("  help                   Display this help message");
+                println!("  exit                   Exit the program");
             }
-            cmd => println!("Unknown command: {}", cmd),
+            cmd => {
+                if cmd.starts_with("reliability ") {
+                    if let Ok(value) = cmd.split_once(' ').unwrap().1.parse() {
+                        reliability = value;
+                        let addresses: Vec<_> = tasks.keys().copied().collect();
+                        for address in addresses {
+                            stop_server!(address);
+                            start_server!(address);
+                        }
+                        println!("Reliability set to {}", reliability);
+                    } else {
+                        println!("Invalid reliability value");
+                    }
+                } else {
+                    println!("Unknown command: {}", cmd);
+                }
+            }
         }
 
         input.clear();
@@ -110,6 +129,7 @@ async fn main() {
 
 struct HealthImpl {
     pub address: SocketAddr,
+    pub reliability: f64,
 }
 
 #[tonic::async_trait]
@@ -119,9 +139,13 @@ impl Health for HealthImpl {
         _request: tonic::Request<()>,
     ) -> Result<tonic::Response<example_protobuf::IsAliveResponse>, tonic::Status> {
         sleep(Duration::from_millis(random_range(200..1000))).await;
-        Ok(tonic::Response::new(example_protobuf::IsAliveResponse {
-            message: format!("I'm alive! (from: {:?})", self.address),
-        }))
+        if rand::rng().random_bool(self.reliability) {
+            Ok(tonic::Response::new(example_protobuf::IsAliveResponse {
+                message: format!("I'm alive! (from: {:?})", self.address),
+            }))
+        } else {
+            Err(tonic::Status::internal("Server internal error"))
+        }
     }
 }
 
